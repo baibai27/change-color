0ef71cd60f0f848244f7ab08a650cbe46f2dee94
Nicole Bai <baijingwen1127@gmail.com>
2023-06-09 19:59:54 -0400

    bmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,66 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
-const colors = [
-  "crimson",
-  "deeppink",
-  "deepskyblue",
-  "gold",
-  "lightgrey",
-  "pink",
-  "orange",
-  "mediumturquoise",
-  "lemonchiffon",
-  "indigo",
-];
-
 const App = () => {
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  useEffect(() => {
-    document.body.style.backgroundColor = colors[randomIndex];
-  }, []);
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [statement, setStatement] = useState("");
 
-  const [colorIndex, setColorIndex] = useState(randomIndex);
-  const [active, setActive] = useState(false);
+  const calculateBMI = (height, weight) => {
+    let bmi = weight / (height / 100) ** 2;
+    bmi = bmi.toFixed(2);
+    // let result = "";
+    // if (bmi < 18.5) {
+    //   result = "Underweight";
+    // } else if (bmi >= 18.5 && bmi < 25) {
+    //   result = "Normal weight";
+    // } else if (bmi >= 25 && bmi < 30) {
+    //   result = "Overweight";
+    // } else if (bmi >= 30) {
+    //   result = "Obesity";
+    // }
+    // const statement = `${bmi}. You are ${result}.`;
+    setStatement(bmi);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    calculateBMI(height, weight);
+  };
 
   return (
-    <div id="app">
-      <h1 id="current-color">
-        {colors[colorIndex].charAt(0).toUpperCase() +
-          colors[colorIndex].slice(1)}
-      </h1>
-      <div className="colors">
-        {colors.map((color, index) => (
-          <button
-            key={`color-${index}`}
-            style={{ backgroundColor: color }}
-            onClick={() => {
-              document.body.style.backgroundColor = colors[index];
-              setColorIndex(index);
-              setActive(true);
-            }}
-            className={active && colorIndex === index ? "active" : ""}
-          ></button>
-        ))}
-      </div>
+    <div className="container">
+      <h1>Calculate BMI</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="height">Height:</label>
+          <input
+            name="height"
+            type="text"
+            className="form-control"
+            placeholder="cm"
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="weight">Weight:</label>
+          <input
+            name="weight"
+            type="text"
+            className="form-control"
+            placeholder="kg"
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+
+        <button className="btn btn-success w-100" type="submit">
+          Calculate
+        </button>
+      </form>
+
+      <p id="result">{statement}</p>
     </div>
   );
 };
